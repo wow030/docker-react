8749c6f91f86bb73c8b588f96959c24d8c366911
wow030 <zaq05206@gmail.com>
2019-12-08 14:38:35 +0800

    fix test case

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was changed on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
